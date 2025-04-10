3c3b635c32089d1cd1841759a50dfbebb56adacc
unknown <youngscientist.iysa@gmail.com>
2025-04-10 13:30:42 +0700

    update kategori -k

--- src/component/category.jsx
+++ src/component/category.jsx
@@ -1,77 +1,77 @@
 const Category = () => {
-    return (
-        <>
-        <section>
-            <div id="rs-courses-categories" className="rs-courses-categories pt-100 pb-70">
-                <div className="container">
-                <div className="sec-title-2 mb-50 after-style">
-                    <h2><span className="orange-color "/>Kategori</h2>
-                    <div className="view-more">
-                    {/* <!-- <a href="#">View All Courses <i className="fa fa-angle-double-right"></i></a> --> */}
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-lg-3 col-md-6">
-                    <div className="courses-item2">
-                        <i className="flaticon-book-1"></i>
-                        <h4 className="courses-title"><a >Games</a></h4>
-                    </div>
-                    </div>
-                    <div className="col-lg-3 col-md-6">
-                    <div className="courses-item2">
-                        <i className="flaticon-tool-1"></i>
-                        <h4 className="courses-title"><a >Life Sciences <br/><br/></a></h4>
-                    </div>
-                    </div>
-                    <div className="col-lg-3 col-md-6">
-                    <div className="courses-item2">
-                        <i className="flaticon-document"></i>
-                        <h4 className="courses-title"><a >Environmental Science<br/><br/></a></h4>
-                    </div>
-                    </div>
-                    <div className="col-lg-3 col-md-6">
-                    <div className="courses-item2">
-                        <i className="flaticon-tool-2"></i>
-                        <h4 className="courses-title"><a >Mechanical and Electrical Engineering</a></h4>
-                    </div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-lg-3 col-md-6">
-                    <div className="courses-item2">
-                        <i className="flaticon-ribbon"></i>
-                        <h4 className="courses-title"><a >Computer Science<br/><br/></a></h4>
-                    </div>
-                    </div>
-                    <div className="col-lg-3 col-md-6">
-                    <div className="courses-item2">
-                        <i className="flaticon-school-1"></i>
-                        <h4 className="courses-title"><a >Social Science<br/><br/></a></h4>
-                    </div>
-                    </div>
-                    <div className="col-lg-3 col-md-6">
-                    <div className="courses-item2">
-                        <i className="flaticon-book"></i>
-                        <h4 className="courses-title"><a >Science in Islam<br/><br/></a></h4>
-                    </div>
-                    </div>
-                    <div className="col-lg-3 col-md-6">
-                    <div className="courses-item2">
-                        <i className="flaticon-graduation"></i>
-                        <h4 className="courses-title"><a >Education<br/><br/></a></h4>
-                    </div>
-                    </div>
-                    <div className="col-lg-3 col-md-6">
-                    <div className="courses-item">
-                        <i className="flaticon-tool"></i>
-                        <h4 className="courses-title"><a >Animation</a></h4>
-                    </div>
-                    </div>
-                </div>
-                </div>
+  return (
+    <>
+      <section>
+        <div
+          id="rs-courses-categories"
+          className="rs-courses-categories pt-100 pb-70"
+        >
+          <div className="container">
+            <div className="sec-title-2 mb-50 after-style">
+              <h2>
+                <span className="orange-color " />
+                Kategori
+              </h2>
+              <div className="view-more">
+                {/* <!-- <a href="#">View All Courses <i className="fa fa-angle-double-right"></i></a> --> */}
+              </div>
             </div>
-        </section>
-        </>
-    )
-}
-export default Category
+            <div className="row">
+              <div className="col-lg-3 col-md-6">
+                <div className="courses-item2">
+                  <i class="fa-solid fa-flask fa-6x"></i>
+                  <h4 className="courses-title">
+                    <a>Pure Science</a>
+                  </h4>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6">
+                <div className="courses-item2">
+                  <i class="fa-solid fa-people-arrows fa-6x"></i>
+                  <h4 className="courses-title">
+                    <a>
+                      Social Science
+                    </a>
+                  </h4>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6">
+                <div className="courses-item2">
+                  <i class="fa-solid fa-book-open-reader fa-6x"></i>
+                  <h4 className="courses-title">
+                    <a>
+                      Education
+                    </a>
+                  </h4>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6">
+                <div className="courses-item2">
+                  <i class="fa-solid fa-book-quran fa-6x"></i>
+                  <h4 className="courses-title">
+                    <a>Science In Islam</a>
+                  </h4>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-lg-3 col-md-6">
+                <div className="courses-item2">
+                  <i class="fa-solid fa-microscope fa-6x"></i>
+                  <h4 className="courses-title">
+                    <a>
+                      Applied Life Science
+                      <br />
+                      <br />
+                    </a>
+                  </h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+export default Category;
