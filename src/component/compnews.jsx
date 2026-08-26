8701089e6829d6819975fb077fbeedf885811431
iysa <youngscientist.iysa@gmail.com>
2026-08-26 22:13:11 +0700

    up news

--- src/component/compnews.jsx
+++ src/component/compnews.jsx
@@ -29,36 +29,77 @@ const Compnews = () => {
                     
                     
 
-                    <div class="row">
-                    <h2 class="text-center mt-50" >Berita 2025</h2>
-                    <div class="card-news mt-5 mx-auto col-lg-3 col-md-12">
+                    <h2 class="text-center mt-50">Berita 2026</h2>
+                    <div class="row justify-content-center">
+                        <div class="card-news mt-5 mx-auto col-lg-3 col-md-12">
+                            <img src="./assets/news/2026-1.jpg" class="card-img-top" alt="..." />
+                            <div class="card-news-body">
+                                <h5 class="card-news-title">SMPIT As-Syifa Boarding School Raih Medali Emas pada NICYMS 2026</h5>
+                                <p class="card-text">SMPIT As-Syifa Boarding School kembali menorehkan prestasi membanggakan di tingkat nasional. Tim inovator muda...</p>
+                                <a href="https://smpit.assyifa-boardingschool.sch.id/smpit-as-syifa-boarding-school-raih-medali-emas-pada-national-invention-competition-for-young-moslem-scientist-nicyms-2026/" target="_blank" rel="noopener noreferrer" class="btn btn-primary">Selengkapnya</a>
+                            </div>
+                        </div>
+                        <div class="card-news mt-5 mx-auto col-lg-3 col-md-12">
+                            <img src="./assets/news/2026-2.jpg" class="card-img-top" alt="..." />
+                            <div class="card-news-body">
+                                <h5 class="card-news-title">MAN 2 Kota Makassar Raih Gold dan Silver Medal Indonesian Young Scientist Association</h5>
+                                <p class="card-text">Siswa MAN 2 Kota Makassar peraih gold dan silver medal pada International Invention Competition for Young Modern Scientist (IICYMS) dan Silver Medal pada (NICYMS)...</p>
+                                <a href="https://kemenag.go.id/internasional/man-2-kota-makassar-raih-gold-dan-silver-medal-indonesian-young-scientist-association-H6a24" target="_blank" rel="noopener noreferrer" class="btn btn-primary">Selengkapnya</a>
+                            </div>
+                        </div>
+                        <div class="card-news mt-5 mx-auto col-lg-3 col-md-12">
+                            <img src="./assets/news/2026-3.jpg" class="card-img-top" alt="..." />
+                            <div class="card-news-body">
+                                <h5 class="card-news-title">Silver Medal pada National Invention Competition for Young Modern Scientist (NICYMS) 2026</h5>
+                                <p class="card-text">Kepala MAN 2 Kota Makassar berfoto Bersama para murid MAN 2 Kota Makassar peraih gold dan silver medal pada...</p>
+                                <a href="https://independenews.com/tag/silver-medal-pada-national-invention-competition-for-young-modern-scientist-nicyms-2026/" target="_blank" rel="noopener noreferrer" class="btn btn-primary">Selengkapnya</a>
+                            </div>
+                        </div>
+                        <div class="card-news mt-5 mx-auto col-lg-3 col-md-12">
+                            <img src="./assets/news/2026-4.png" class="card-img-top" alt="..." />
+                            <div class="card-news-body">
+                                <h5 class="card-news-title">Congratulations, Winners! National Invention Competition for Young Moslem Scientist</h5>
+                                <p class="card-text">Selamat kepada para pemenang National Invention Competition for Young Moslem Scientist (NICYMS) atas prestasi yang telah diraih...</p>
+                                <a href="https://www.instagram.com/p/DacfRw5GMH7/?utm_source=ig_web_button_share_sheet" target="_blank" rel="noopener noreferrer" class="btn btn-primary">Selengkapnya</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <h2 class="text-center mt-50">Berita 2025</h2>
+                    <div class="row justify-content-center">
+                        <div class="card-news mt-5 mx-auto col-lg-3 col-md-12">
                             <img src="./assets/news/20252.jpg" class="card-img-top" alt="..." />
                             <div class="card-news-body">
                                 <h5 class="card-news-title">Sukses Digelar, NICYMS dan IICYMS 2025 Kembali Hadir di UNIKOM</h5>
                                 <p class="card-text">Jabaran.id – Indonesia Young Scientist Association (IYSA) kembali sukses menyelenggarakan dua ajang bergengsi bertaraf internasional, yaitu National...</p>
-                                <a href="https://jabaran.id/sukses-digelar-nicyms-dan-iicyms-2025-kembali-hadir-di-unikom/" class="btn btn-primary">Selengkapnya</a>
+                                <a href="https://jabaran.id/sukses-digelar-nicyms-dan-iicyms-2025-kembali-hadir-di-unikom/" target="_blank" rel="noopener noreferrer" class="btn btn-primary">Selengkapnya</a>
                             </div>
                         </div>
-                    <div class="card-news mt-5 mx-auto col-lg-3 col-md-12">
+                        <div class="card-news mt-5 mx-auto col-lg-3 col-md-12">
                             <img src="./assets/news/20251.webp" class="card-img-top" alt="..." />
                             <div class="card-news-body">
                                 <h5 class="card-news-title">Ratusan Siswa dari 20 Negara Ikuti IICYMS dan NICYMS serta Olimpiade Robotik di Unikom Bandung</h5>
                                 <p class="card-text">PIKIRAN RAKYAT - Ratusan siswa dari 20 negara mengikuti International Invention Competition for Young Moslem Scientists (IICYMS) dan National Invention...</p>
-                                <a href="https://www.pikiran-rakyat.com/pendidikan/pr-019530695/ratusan-siswa-dari-20-negara-ikuti-iicyms-dan-nicyms-serta-olimpiade-robotik-di-unikom-bandung" class="btn btn-primary">Selengkapnya</a>
+                                <a href="https://www.pikiran-rakyat.com/pendidikan/pr-019530695/ratusan-siswa-dari-20-negara-ikuti-iicyms-dan-nicyms-serta-olimpiade-robotik-di-unikom-bandung" target="_blank" rel="noopener noreferrer" class="btn btn-primary">Selengkapnya</a>
                             </div>
                         </div>
-                    <h2 class="text-center mt-50" >Berita 2024</h2>
-                    <div class="card-news mt-5 mx-auto col-lg-3 col-md-12">
+                    </div>
+
+                    <h2 class="text-center mt-50">Berita 2024</h2>
+                    <div class="row justify-content-center">
+                        <div class="card-news mt-5 mx-auto col-lg-3 col-md-12">
                             <img src="./assets/news/20241.webp" class="card-img-top" alt="..." />
                             <div class="card-news-body">
                                 <h5 class="card-news-title">Ajang Sains Nasional dan Internasional di UNIKOM: Pembuktian Pelajar Muslim di Era Modern</h5>
                                 <p class="card-text">MATA BANDUNG - Universitas Komputer Indonesia (UNIKOM) kembali membuktikan kiprahnya dalam dunia pendidikan...</p>
-                                <a href="https://matabandung.pikiran-rakyat.com/pendidikan/pr-1828496552/ajang-sains-nasional-dan-internasional-di-unikom-pembuktian-pelajar-muslim-di-era-modern?page=all" class="btn btn-primary">Selengkapnya</a>
+                                <a href="https://matabandung.pikiran-rakyat.com/pendidikan/pr-1828496552/ajang-sains-nasional-dan-internasional-di-unikom-pembuktian-pelajar-muslim-di-era-modern?page=all" target="_blank" rel="noopener noreferrer" class="btn btn-primary">Selengkapnya</a>
                             </div>
                         </div>
+                    </div>
 
-                    <h2 class="text-center mt-50" >Berita 2023</h2>
-                    <div class="card-news mt-5 mx-auto col-lg-3 col-md-12">
+                    <h2 class="text-center mt-50">Berita 2023</h2>
+                    <div class="row justify-content-center">
+                        <div class="card-news mt-5 mx-auto col-lg-3 col-md-12">
                             <img src="./assets/news/news5.jpg" class="card-img-top" alt="..." />
                             <div class="card-news-body">
                                 <h5 class="card-news-title">Ciptakan Aplikasi untuk Pelajar Disabilitas, 2 Siswa Madrasah Raih Emas di Ajang IICYMS 2023</h5>
@@ -74,9 +115,10 @@ const Compnews = () => {
                                 <a href="/news5" class="btn btn-primary">Selengkapnya</a>
                             </div>
                         </div>
-                        
+                    </div>
 
-                        <h2 class="text-center">Berita 2022</h2>
+                    <h2 class="text-center mt-50">Berita 2022</h2>
+                    <div class="row justify-content-center">
                         <div class="card-news mt-5 mx-auto col-lg-3 col-md-12">
                             <img src="./assets/news/news1.jpeg" class="card-img-top" alt="..." />
                             <div class="card-body">
@@ -85,8 +127,7 @@ const Compnews = () => {
                                 <a href="/news1" class="btn btn-primary">Selengkapnya</a>
                             </div>
                         </div>
-
-                        <div class="card-news mt-5 mx-auto col-lg-3 col-md-12" >
+                        <div class="card-news mt-5 mx-auto col-lg-3 col-md-12">
                             <img src="./assets/news/news2.jpg" class="card-img-top" alt="..." />
                             <div class="card-news-body">
                                 <h5 class="card-news-title">UB Student Team Won Award at the 2022 IICYMS International Competition</h5>
@@ -97,13 +138,11 @@ const Compnews = () => {
                         <div class="card-news mt-5 mx-auto col-lg-3 col-md-12">
                             <img src="./assets/news/news3.jpeg" class="card-img-top" alt="..." />
                             <div class="card-news-body">
-                                <h5 class="card-news-title">Tim Unila Raih Medali Perak pada International Invention
-                                    Competition for Young Moslem Scientists (IICYMS) 2022</h5>
+                                <h5 class="card-news-title">Tim Unila Raih Medali Perak pada International Invention Competition for Young Moslem Scientists (IICYMS) 2022</h5>
                                 <p class="card-text"> (Unila): Tim Universitas Lampung (Unila) yang diketuai Indah Sukma Ningsih (Biologi 2018) dan beranggotakan Raihan Adhiyatma Atalla (Biologi 2019), Al Khasanah (Teknologi Hasil Pertanian 2019), Susanto (Agronomi 2018), dan Renaldy Jovanda (Pendidikan Sejarah 2019) berhasil meraih medali perak...</p>
                                 <a href="/news3" class="btn btn-primary">Selengkapnya</a>
                             </div>
                         </div>
-                        
                     </div>
                 </div>
             </div>
